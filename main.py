c8ec3c7cb9e13d9153043f2425ca8e343d35e5bf
Nikita <nikita.efan.2017@gmail.com>
2023-05-10 14:25:37 +0300

    fix

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_headers=["*"],
-    allow_methods=['POST', 'GET'],
+    allow_methods=['POST', 'GET', 'PATCH'],
     allow_credentials=True,
 )
 
